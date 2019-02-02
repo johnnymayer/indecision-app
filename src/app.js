a9797d59b2754ce0ac34196c7e5edc0179387064
johnnymayer <johnnymayer@gmail.com>
2019-02-01 23:21:15 -0800

    Clean setState calls

--- src/app.js
+++ src/app.js
@@ -10,11 +10,7 @@ class IndecisionApp extends React.Component {
   }
 
   handleDeleteOptions() {
-    this.setState(() => {
-      return {
-        options: [],
-      };
-    });
+    this.setState(() => ({ options: [], }));
   }
 
   handlePick() {
@@ -30,11 +26,9 @@ class IndecisionApp extends React.Component {
       return 'This option already exists.';
     }
 
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.concat(option)
-      };
-    });
+    this.setState((prevState) => ({ 
+      options: prevState.options.concat(option)
+    }));
   }
 
   render() {
@@ -123,9 +117,7 @@ class AddOption extends React.Component {
     const option = e.target.elements.option.value.trim();
     const err = this.props.handleAddOption(option);
 
-    this.setState(() => {
-      return { err }
-    });
+    this.setState(() => ({err}));
   }
 
   render() {
