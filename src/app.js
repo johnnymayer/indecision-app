63ec45b6e5a64ce52ca3ceb69540a8d56cef4c95
johnnymayer <johnnymayer@gmail.com>
2019-03-22 20:18:07 -0700

    Add babel-core, add module+rules for babel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-// import './utils.js';
-import { square, add } from './utils.js';
-import * as utils from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is run');
-console.log(square(4));
-console.log(add(1,2));
+const template = <p>TESTING 1 2 3 this is jsx from webpack</p>;
+ReactDOM.render(template, document.getElementById('app'));
