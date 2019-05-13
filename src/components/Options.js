f412e419fc9cbc8dd6ab9e0346dce2ec2389ecb2
johnnymayer <johnnymayer@gmail.com>
2019-05-12 19:44:01 -0700

    Refactor Action, Header, Option, Options

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-    return (
+const Options = (props) => (
       <div>
         <button onClick={props.handleDeleteOptions}>Remove All</button>
         {
@@ -19,6 +18,5 @@ const Options = (props) => {
         }
       </div>
     );
-  };
 
   export default Options;
